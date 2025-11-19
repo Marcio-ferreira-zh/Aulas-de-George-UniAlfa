cb7646e2d7de11e6bf5e8f8b4062e8a278a4d42d
Márcio <marcioferreirazh@gmail.com>
2025-11-19 19:49:58 -0300

    Update Quest9.java

--- PooJava/Quests_29_10/Quest9.java
+++ PooJava/Quests_29_10/Quest9.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class Quest9 {
     public static void main (String[] args ){
-        System.out.println("Em andamento");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escreva o número 1");
+        int n1 = sc.nextInt();
+        if (n1 % 2 == 0){
+        System.out.println("Seu número é par");
+        }else{
+            System.out.println("Seu número é impar");
+        }
+   
+       sc.close();  
     }
 }
